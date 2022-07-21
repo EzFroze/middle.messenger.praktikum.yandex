f7e08a144f9f4e581c34c74d57882bfd9049c955
rusultanbekov <rusultanbekov@ozon.ru>
2022-07-21 21:25:10 +0300

    Функция для получение темплейта по урлу

---
+++ src/utils/get-template.js
@@ -0,0 +1,20 @@
+import { urls } from "../const";
+
+export function getTemplate() {
+  let pathname = window.location.pathname;
+
+  if (!pathname) return;
+
+  if (pathname.length === 1) {
+    window.location.href = "/auth"; // Временное решение чтобы попадать на авторизацию
+  }
+
+  if (pathname.length > 1) {
+    pathname = pathname.slice(1);
+    if (!urls[pathname]) {
+      window.location.href = "/404";
+    }
+  }
+
+  return urls[pathname];
+}
