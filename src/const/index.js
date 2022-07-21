4aab3ab644a9287022451fd78283f60260d1da9c
rusultanbekov <rusultanbekov@ozon.ru>
2022-07-21 21:24:41 +0300

    Сделал константу для урлов

---
+++ src/const/index.js
@@ -0,0 +1,8 @@
+import { authPage } from "../pages/exports";
+import { registerPage } from "../pages/exports";
+
+export const urls = {
+  auth: authPage,
+  register: registerPage,
+  404: authPage,
+};
