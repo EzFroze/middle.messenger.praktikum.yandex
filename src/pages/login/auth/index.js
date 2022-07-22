c2affcf5a559e8b3033d6fdefa2a555ff9cb26f6
rusultanbekov <rusultanbekov@ozon.ru>
2022-07-22 23:03:49 +0300

    Сверстал страницы авторизации и регистрации

--- src/pages/login/auth/index.js
+++ src/pages/login/auth/index.js
@@ -1,7 +1,12 @@
 import style from "./styles.module.pcss";
 import template from "./index.hbs";
 import { loginLayout } from "../../../layout/login";
+import { input } from "../../../components/exports";
 
-const result = template({ style });
+const result = template({
+  style,
+  loginInput: input("Логин", "text", "login", true, true),
+  passwordInput: input("Пароль", "password", "password", false, true),
+});
 
 export const authPage = loginLayout(result);
