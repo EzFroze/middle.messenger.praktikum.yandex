deb54ffbd9775377b737ebcdfe9cf48de5402772
rusultanbekov <rusultanbekov@ozon.ru>
2022-07-23 17:14:11 +0300

    Добавил express сервер

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.use((_, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
