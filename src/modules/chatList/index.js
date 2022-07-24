33cf96306dbe5331873c45e734de693cf1401336
rusultanbekov <rusultanbekov@ozon.ru>
2022-07-24 16:15:22 +0300

    Сверстал блок с списком чатов

--- src/modules/chatList/index.js
+++ src/modules/chatList/index.js
@@ -1,6 +1,8 @@
 import template from "./index.hbs";
 import * as style from "./styles.module.pcss";
 
+import arrow from "../../../static/images/arrow.svg";
+
 export const chatList = (chats) => {
-  return template({ style, chats });
+  return template({ style, arrow, chats });
 };
