ee6183934ea45f60f8f80c3545dc2fb9cdf59019
rustam.sultanbekov <rustam.sultanbekov@aliexpress.ru>
2022-09-24 23:18:32 +0300

    styling

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -1,24 +1,27 @@
-import style from "./styles.module.pcss";
-import template from "./index.hbs";
 import Block, { TProps } from "../../app/block";
+import template from "./index.hbs";
+import style from "./styles.module.pcss";
 
 type Props = {
-  style?: typeof style,
-  label?: string,
-  placeholder?: string,
-  type: string,
-  id: string,
-  autofocus?: boolean,
-  required?: boolean,
-  value?: string,
-  error?: string
+  style?: typeof style;
+  label?: string;
+  placeholder?: string;
+  type: string;
+  id: string;
+  autofocus?: boolean;
+  required?: boolean;
+  value?: string;
+  error?: string;
 } & TProps;
 
-const defaultValues: Pick<Props, "autofocus" | "required" | "style" | "value"> = {
+const defaultValues: Pick<
+  Props,
+  "autofocus" | "required" | "style" | "value" | "error"
+> = {
   autofocus: false,
   required: false,
   style,
-  value: ""
+  value: "",
 };
 
 export class Input extends Block<Props> {
